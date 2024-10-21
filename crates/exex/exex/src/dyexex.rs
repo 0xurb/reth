18dcf2f7718a8d6ebd91ffda6afd294e2c883f0d
0xurb <urbadeil@gmail.com>
2024-10-21 22:13:43 +0100

    chore(exex): `define_exex` macro tests

--- crates/exex/exex/src/dyexex.rs
+++ crates/exex/exex/src/dyexex.rs
@@ -39,13 +39,61 @@ macro_rules! define_exex {
     (async move |ctx| {
         Ok($user_fn:ident(ctx))
     }) => {
-	    #[no_mangle]
-	    pub extern fn _launch_exex<Node: FullNodeComponents>(
-	        ctx: $crate::ExExContext<Node>,
-	    ) -> impl std::future::Future<
-	        Output = eyre::Result<impl std::future::Future<Output = eyre::Result<()>> + Send>,
-	    > {
-			async move { Ok($user_fn(ctx)) }
-	    }
+        #[no_mangle]
+        pub extern fn _launch_exex<Node: FullNodeComponents>(
+            ctx: $crate::ExExContext<Node>,
+        ) -> impl std::future::Future<
+            Output = eyre::Result<impl std::future::Future<Output = eyre::Result<()>> + Send>,
+        > {
+            async move { Ok($user_fn(ctx)) }
+        }
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use reth_exex_test_utils::{test_exex_context, Adapter};
+    use reth_node_api::FullNodeComponents;
+
+    use crate::ExExContext;
+
+    #[tokio::test]
+    async fn should_define_exex() -> eyre::Result<()> {
+        async fn exex<Node: FullNodeComponents>(
+            _ctx: ExExContext<Node>,
+        ) -> eyre::Result<impl std::future::Future<Output = eyre::Result<()>>> {
+            let _exex = async move { Ok(()) };
+            Ok(_exex)
+        }
+
+        define_exex!(exex);
+
+        let (ctx, _) = test_exex_context().await?;
+
+        _launch_exex::<Adapter>(ctx);
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn should_define_exex_closure() -> eyre::Result<()> {
+        // ensure that we have right `exex` closure by design
+        async fn _exex<Node: FullNodeComponents>(
+            ctx: ExExContext<Node>,
+        ) -> eyre::Result<impl std::future::Future<Output = eyre::Result<()>>> {
+            Ok(exex(ctx))
+        }
+
+        async fn exex<Node: FullNodeComponents>(_ctx: ExExContext<Node>) -> eyre::Result<()> {
+            Ok(())
+        }
+
+        define_exex!(async move |ctx| { Ok(exex(ctx)) });
+
+        let (ctx, _) = test_exex_context().await?;
+
+        _launch_exex::<Adapter>(ctx);
+
+        Ok(())
     }
 }
